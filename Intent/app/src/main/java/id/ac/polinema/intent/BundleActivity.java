e303fe480af677a01a37cea60766b40792e75aca
OK <hafiznur312@gmail.com>
2019-09-23 18:10:35 +0700

    oke

--- Intent/app/src/main/java/id/ac/polinema/intent/BundleActivity.java
+++ Intent/app/src/main/java/id/ac/polinema/intent/BundleActivity.java
@@ -8,10 +8,27 @@ import android.view.View;
 import android.widget.EditText;
 
 public class BundleActivity extends AppCompatActivity {
+    public static final String USERNAME_KEY = "username";
+    public static final String NAME_KEY = "name";
+    public static final String AGE_KEY = "age";
+    private EditText usernameInput;
+    private EditText nameInput;
+    private EditText ageInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bundle);
+
+        usernameInput = findViewById(R.id.input_username);
+        nameInput = findViewById(R.id.input_name);
+        ageInput = findViewById(R.id.input_age);
+
+    }
+
+    public void handleSubmit(View view) {
+        String username = usernameInput.getText().toString();
+        String name = nameInput.getText().toString();
+        int age = Integer.parseInt(ageInput.getText().toString());
     }
 }
--- Intent/app/src/main/java/id/ac/polinema/intent/ExplicitIntentActivity.java
+++ Intent/app/src/main/java/id/ac/polinema/intent/ExplicitIntentActivity.java
@@ -9,10 +9,21 @@ import android.widget.TextView;
 
 public class ExplicitIntentActivity extends AppCompatActivity {
 
+    EditText nameInput;
+    TextView teksView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_explicit_intent);
+        nameInput = findViewById(R.id.input_name);
+        teksView = findViewById(R.id.text_output);
     }
 
+
+    public void button(View view) {
+        String name = nameInput.getText().toString();
+        teksView.setText("Hello " + name);
+    }
 }
+
